7f201e3071162261480f729b73086f5a68a4834e
Arij <arij.lemjid@etu.unice.fr>
2025-02-01 23:30:10 +0100

    tables modif

--- tables_creation.sql
+++ tables_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE Type_logement (
     charge_supplementaire FLOAT CHECK (charge_supplementaire >= 0)
 );
 
-CREATE TABLE IF NOT EXISTS Logement (
+CREATE TABLE Logement (
     id_logement SERIAL PRIMARY KEY,
     type_logement VARCHAR(50) NOT NULL,
     emplacement VARCHAR(100) NOT NULL,
@@ -12,17 +12,10 @@ CREATE TABLE IF NOT EXISTS Logement (
     loyer FLOAT CHECK (loyer >= 0),
     nombre_chambres INT CHECK (nombre_chambres > 0),
     id_type_logement INT NOT NULL,
-    --cle candidta de type_logement
     CONSTRAINT fk__type_logement FOREIGN KEY (id_type_logement)
         REFERENCES Type_logement (id_type_logement)
         ON DELETE CASCADE
 );
-
-CREATE TABLE Type_Equipement (
-    id_type_equip SERIAL PRIMARY KEY,
-    type_Equipement VARCHAR(50) NOT NULL
-);
-
 CREATE TABLE Equipement (
     id_equipement SERIAL PRIMARY KEY,
     nom_equipement VARCHAR(100) NOT NULL,
@@ -30,10 +23,6 @@ CREATE TABLE Equipement (
     id_logement INT NOT NULL,
     CONSTRAINT fk_logement FOREIGN KEY (id_logement)
         REFERENCES Logement (id_logement)
-        ON DELETE CASCADE,
-     id_type_equip INT NOT NULL,
-    CONSTRAINT fk__type_equip FOREIGN KEY (id_type_equip)
-        REFERENCES Type_Equipement (id_type_equip)
         ON DELETE CASCADE
 );
 
@@ -54,25 +43,15 @@ CREATE TABLE Profil (
     
 );
 
-CREATE TABLE Personne (
-    id_personne SERIAL PRIMARY KEY,
-    nom_pers VARCHAR(50) NOT NULL,
-    prenom_pers VARCHAR(50) NOT NULL,
-    cin VARCHAR(20) UNIQUE NOT NULL,
-    date_naissance DATE,
-    num_tel VARCHAR(20),
-    email VARCHAR(50)
-);
-
 CREATE TABLE Resident (
     id_resident SERIAL PRIMARY KEY,
-    id_personne INT NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    date_naissance DATE NOT NULL,
+    cin VARCHAR(20) UNIQUE NOT NULL,
     date_entree DATE NOT NULL,
     date_sortie DATE,
     id_profil INT NOT NULL,
-     CONSTRAINT fk_personne_resident FOREIGN KEY (id_personne)
-        REFERENCES Personne (id_personne)
-        ON DELETE CASCADE,
     CONSTRAINT fk_id_profil FOREIGN KEY (id_profil)
         REFERENCES Profil (id_profil)
         ON DELETE CASCADE
@@ -93,15 +72,6 @@ CREATE TABLE Reservation (
         ON DELETE CASCADE
 );
 
-create table Facture(
-    id_facture int not null,
-    cin_pers VARCHAR(30) not null,
-    prix_total DECIMAL not null,
-    id_reservation int not null,
-    CONSTRAINT fk_id_res FOREIGN KEY (id_reservation)
-        REFERENCES Reservation (id_reservation)
-        ON DELETE CASCADE
-);
 
 CREATE TABLE Type_intervention (
     id_type_intervention SERIAL PRIMARY KEY,
@@ -117,13 +87,8 @@ CREATE TABLE Intervention (
     id_type_intervention INT NOT NULL,
     CONSTRAINT fk_id_type_intervention FOREIGN KEY (id_type_intervention)
         REFERENCES Type_intervention (id_type_intervention)
-        ON DELETE CASCADE,
-    id_logement INT NOT NULL,
-    CONSTRAINT fk_id_logement FOREIGN KEY (id_logement)
-        REFERENCES Logement (id_logement)
         ON DELETE CASCADE
 );
-
 
 
 
@@ -133,13 +98,10 @@ CREATE TABLE Conflit (
     resolu VARCHAR(10)  CHECK (resolu in ('oui', 'En cours','non')),
     date_conflit DATE NOT NULL,
     id_resident INT NOT NULL
-    /*CONSTRAINT fk_resident_conflit FOREIGN KEY (id_resident)
-        REFERENCES Resident (id_resident)
-        ON DELETE CASCADE*/
 );
 
 
-CREATE TABLE resident_conflicts (
+CREATE TABLE Resident_conflicts (
     id SERIAL PRIMARY KEY,
     id_conflit INT NOT NULL,
     id_resident INT NOT NULL,
@@ -154,11 +116,6 @@ CREATE TABLE Evenement (
     type_evenement VARCHAR(50) NOT NULL,
     date_event DATE NOT NULL
 );
-
-
-
-
-
 
 CREATE TABLE Participation (
     id_resident INT NOT NULL,
